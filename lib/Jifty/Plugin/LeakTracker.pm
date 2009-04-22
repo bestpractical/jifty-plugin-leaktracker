4f5032cbb28a122365a619ddbe19a796a8a05706
Shawn Moore <sartak@bestpractical.com>
2009-04-22 09:04:45 +0000

    Update copyright year
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/plugins/Jifty-Plugin-LeakTracker@6826 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/LeakTracker.pm
+++ lib/Jifty/Plugin/LeakTracker.pm
@@ -88,7 +88,7 @@ L<Jifty::Plugin::Gladiator>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 Best Practical Solutions
+Copyright 2007-2009 Best Practical Solutions
 
 This is free software and may be modified and distributed under the same terms as Perl itself.
 
